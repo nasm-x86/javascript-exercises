2fde2164224ca197b5d63ade3ffc31f550cb8f86
nasm-x86 <nasm.x86@yandex.ru>
2021-09-28 17:00:52 +0400

    Add solution for 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
 const reverseString = function(str) {
-
+  let charArray = str.split('');
+  let newStr = [];
+  for (i = charArray.length; i >= 0; i--) {
+    newStr.push(charArray[i]);
+  }
+  return newStr.join('');
 };
 
 // Do not edit below this line
