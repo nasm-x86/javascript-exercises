d350143c73ace8aae3cfae08884af723be12f775
nasm-x86 <nasm.x86@yandex.ru>
2021-09-28 17:32:38 +0400

    Add solution for 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, element) {
+  let args = Array.from(arguments);
+  args = args.slice(1, arguments.length);
+  for (let i = array.length; i >= 0; i--) {
+    for (let n = args.length; n >= 0; n--) {
+      if (array[i] === args[n]) {
+        array.splice(i, 1);
+      }
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
