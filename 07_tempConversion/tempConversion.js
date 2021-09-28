9a2c08d8ba88774173a29bbb97cfbfb3e91cb3ae
nasm-x86 <nasm.x86@yandex.ru>
2021-09-28 19:09:03 +0400

    Add solution for 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f_temp) {
+  let c_temp = (f_temp - 32) * 5 / 9;
+  c_temp = Math.round(c_temp * 10) / 10;
+  return c_temp;
 };
 
-const ctof = function() {
-
+const ctof = function(c_temp) {
+  let f_temp = c_temp * 9 / 5 + 32;
+  f_temp = Math.round(f_temp * 10) / 10;
+  return f_temp;
 };
 
 // Do not edit below this line
