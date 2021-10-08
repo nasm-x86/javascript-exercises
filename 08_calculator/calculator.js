aa70cf3abf9bc62d22692a418ee3b7c3e05cc5b5
nasm-x86 <nasm.x86@yandex.ru>
2021-10-08 15:36:33 +0400

    Add solution for 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  for (let i in array) {
+    sum += array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = array[0];
+  for (let i in array.slice(1)) {
+    result *= array.slice(1)[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	result = a;
+  for (let i = a - 1; i > 0; i--) {
+    result *= i;
+  }
+  if (a === 0) {
+    return 1;
+  } else {
+  return result;
+  }
 };
 
 // Do not edit below this line
