1738bc63faf5d4100d7c9ed6a979679e3c8e3a17
nasm-x86 <nasm.x86@yandex.ru>
2021-10-09 13:21:32 +0400

    Add solution for 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+  array.sort(function(a, b) {
+    let lastGuy;
+    let nextGuy;
+    const currentYear = new Date().getFullYear();
+    if ('yearOfDeath' in a) {
+      lastGuy = a.yearOfDeath - a.yearOfBirth;
+    } else {
+      lastGuy = currentYear - a.yearOfBirth;
+    }
+    if ('yearOfDeath' in b) {
+      nextGuy = b.yearOfDeath - b.yearOfBirth;
+    } else {
+      nextGuy = currentYear - b.yearOfBirth;
+    }
+    return lastGuy > nextGuy ? -1: 1;
+  });
 
+  return array[0];
 };
 
 // Do not edit below this line
