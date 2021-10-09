973348c3c418666ff5361602cff73c62c6dc3049
nasm-x86 <nasm.x86@yandex.ru>
2021-10-09 11:37:29 +0400

    Add solution for 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,30 @@
-const palindromes = function () {
+const palindromes = function(string) {
+  let newStringArray = string.toLowerCase().split('').reverse();
+  for (let i = newStringArray.length - 1; i >= 0; i--) {
+    if (!isLetter(newStringArray[i])) {
+      newStringArray.splice(i, 1);
+    }
+  }
+  let newString = newStringArray.join('');
 
+  let originalStringArray = string.toLowerCase().split('');
+  for (let i = originalStringArray.length - 1; i >= 0; i--) {
+    if (!isLetter(originalStringArray[i])) {
+      originalStringArray.splice(i, 1);
+    }
+  }
+  let originalString = originalStringArray.join('');
+
+  if (newString === originalString) {
+    return true;
+  } else {
+    return false;
+  }
 };
+
+function isLetter(string) {
+  return string.length === 1 && string.match(/[a-z]/i);
+}
 
 // Do not edit below this line
 module.exports = palindromes;
