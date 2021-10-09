24a7c3b8a2d359a59cab57c6ff734e96f7ce216f
nasm-x86 <nasm.x86@yandex.ru>
2021-10-09 12:23:47 +0400

    Add solution for 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  if (typeof n === 'string') {
+    n = Number(n);
+  }
+  
+  if (n < 0) {
+    return "OOPS"
+  } else if (n <= 1) {
+    return n;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
 };
 
 // Do not edit below this line
